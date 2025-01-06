6e77b434e85ef4a0bd7417dc214545bb4ec00ba0
石李城 <985478238@qq.com>
2025-01-06 17:54:57 +0800

    搭建IDEA下的kotlin环境
     搭建IDEA下的kotlin环境，并创了两个示例代码

---
+++ Kotlin_Journey/src/Main.kt
@@ -0,0 +1,14 @@
+class Main {
+    fun main(args : Array<String>){
+        println("kotlin主函数在类的内部将无法识别")
+
+    }
+}
+
+/**
+ *
+ */
+fun main(args : Array<String>){
+    println("kotlin主函数在外部")
+
+}
---
+++ Kotlin_Journey/src/Test.kt
@@ -0,0 +1,6 @@
+class Test {
+}
+fun main(args : Array<String>){
+    println("你好 kotlin")
+
+}
